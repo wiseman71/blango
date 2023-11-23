1a07a2d9eb877c1368ed49abf3672910472a7ea6
Francesco Mansi <wiseman@live.it>
2023-11-23 18:28:06 +0100

    cc

--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
 
 urlpatterns = [
+    path("", blog.views.index),
     path('admin/', admin.site.urls),
 ]
